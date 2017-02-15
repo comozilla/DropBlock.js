00534c7203a6809752bc095f15e6237537241436
nakamurataichi <ringo.m3@icloud.com>
2017-02-15 23:09:38 +0900

    moved eventListener

--- script/blocks-manager.js
+++ script/blocks-manager.js
@@ -7,6 +7,14 @@ var BlocksManager = (function() {
       alertBlock: { hint: "～をアラートする", script: "alert(～)", color: "#0FF" }
     };
     this.element = document.getElementById("workingarea");
+    this.element.addEventListener("dragover", function(e) {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = "copy";
+    });
+    this.element.addEventListener("drop", function(e) {
+      var dropName = e.dataTransfer.getData("text/x-blockName");
+      this.element.textContent = dropName;
+    }.bind(this));
   }
   BlocksManager.prototype.init = function() {
     Object.keys(this.blockConfigs).forEach(function(key) {
--- script/main.js
+++ script/main.js
@@ -2,13 +2,4 @@ document.addEventListener("DOMContentLoaded", function() {
   var bs = new BlocksManager();
   bs.init();
   bs.addBlocks("testBlock", "テスト関数", "function test() {}", "#F0F");
-
-  bs.element.addEventListener("dragover", function(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "copy";
-  });
-  bs.element.addEventListener("drop", function(e) {
-    var dropName = e.dataTransfer.getData("text/x-blockName");
-    bs.element.textContent = dropName;
-  });
 });
